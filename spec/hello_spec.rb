4ad1e92839bebf66b78cc2c8710d188eec494fe0
Steven Nunez <steven.nunez@gmail.com>
2017-02-02 18:08:32 -0500

    Make intention of tests more clear.

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,9 +1,17 @@
 require "spec_helper"
 
 describe "#hello_t" do 
-  it "takes in an argument of an array and puts out a greeting to each person in the array whose name begins with the letter T" do 
-    array = ["Tim", "Tom", "Jim"]
-    expect{hello_t(array){|name| puts "Hi, #{name}" if name.start_with?('T') }}.to output("Hi, Tim\nHi, Tom\n").to_stdout
-    expect(hello_t(array) {|name| puts "Hi, #{name}" if name.start_with?('T') }).to eq(array)
+  let (:names) { ["Tim", "Tom", "Jim"] }
+  it "only passes names that start with 'T' to the block" do 
+    expect{hello_t(names){|name| puts "Hi, #{name}" }}.to output("Hi, Tim\nHi, Tom\n").to_stdout
+  end
+
+  it "returns an array with only names that start with 'T'" do
+    expect(hello_t(names) {|name| puts "Hi, #{name}" }).to eq(["Tim", "Tom"])
+  end
+
+  it "is case insensitive" do
+    other_names = ["tim", "tom", "jim"]
+    expect{hello_t(other_names){|name| puts "Hi, #{name}" }}.to output("Hi, tim\nHi, tom\n").to_stdout
   end
 end
