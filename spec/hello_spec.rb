55c21d1c090aa8ad66695cbada19bb5718db03c2
Gabe Jackson <gj@mail.co.de>
2017-05-31 14:41:34 -0400

    align test expectation w/ Code-Along instructions

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,13 +1,14 @@
 require "spec_helper"
 
-describe "#hello_t" do 
+describe "#hello_t" do
   let (:names) { ["Tim", "Tom", "Jim"] }
-  it "only passes names that start with 'T' to the block" do 
+
+  it "only passes names that start with 'T' to the block" do
     expect{hello_t(names){|name| puts "Hi, #{name}" if name.start_with?("T") }}.to output("Hi, Tim\nHi, Tom\n").to_stdout
   end
 
-  it "returns an array with only names that start with 'T'" do
-    expect(hello_t(names) {|name| puts "Hi, #{name}" if name.start_with?('T') }).to eq(names)
+  it "returns the original array" do
+    expect(hello_t(names) {|name| puts "Hi, #{name}" if name.start_with?("T") }).to eq(names)
   end
 
   it "is case insensitive" do
