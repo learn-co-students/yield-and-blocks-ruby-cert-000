614a502df2db66fbca9ae7ea716b6fa633e2c6ba
Gabe Jackson <gj@mail.co.de>
2017-06-08 11:40:41 -0400

    Simplify and organize tests to improve readability (Issue #33)

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -4,15 +4,21 @@ describe "#hello_t" do
   let (:names) { ["Tim", "Tom", "Jim"] }
 
   it "calls the block once for each element in the passed-in array" do
-    expect{hello_t(names){|name| puts "Hi, #{name}" if name.start_with?("T") }}.to output("Hi, Tim\nHi, Tom\n").to_stdout
+    expect{ hello_t(names){ |name| puts name } }
+      .to output("Tim\nTom\nJim\n")
+      .to_stdout
   end
 
   it "returns the original array" do
-    expect(hello_t(names) {|name| puts "Hi, #{name}" if name.start_with?("T") }).to eq(names)
+    expect( hello_t(names){ |name| puts name } )
+      .to eq(names)
   end
 
   it "is not hard-coded" do
-    other_names = ["tim", "tom", "jim"]
-    expect{hello_t(other_names){|name| puts "Hi, #{name}" if name.start_with?("t") }}.to output("Hi, tim\nHi, tom\n").to_stdout
+    other_names = ["Ali", "Jasmine", "Persephone"]
+
+    expect{ hello_t(other_names){ |name| puts name } }
+      .to output("Ali\nJasmine\nPersephone\n")
+      .to_stdout
   end
 end
