557408344cfbcd877fa4b31c0c7811ec173b9e33
Gabe Jackson <gj@mail.co.de>
2017-06-08 11:43:26 -0400

    Create test for block_given? to improve test coverage and align it with the README

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -21,4 +21,10 @@ describe "#hello_t" do
       .to output("Ali\nJasmine\nPersephone\n")
       .to_stdout
   end
+
+  it "fails gracefully when a block is not passed in" do
+    expect{ hello_t(names) }
+      .to output("Hey! No block was given!\n")
+      .to_stdout
+  end
 end
