f349ef190316928fa4e7240fedd879b0461bf0fa
Gabe Jackson <gj@mail.co.de>
2017-06-08 14:59:53 -0400

    Expand test coverage to guard against a student hard-coding not only the passed-in array but also the block provided to #hello_t

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -17,8 +17,8 @@ describe "#hello_t" do
   it "is not hard-coded" do
     other_names = ["Ali", "Jasmine", "Persephone"]
 
-    expect{ hello_t(other_names){ |name| puts name } }
-      .to output("Ali\nJasmine\nPersephone\n")
+    expect{ hello_t(other_names){ |name| puts name.upcase } }
+      .to output("ALI\nJASMINE\nPERSEPHONE\n")
       .to_stdout
   end
 
